e4db05c46605474283df16474006c3735fcd8a50
yong liang <yongliang24@gmail.com>
2018-11-03 07:33:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,10 +2,11 @@
 function theBeatlesPlay(musician, instrument)
 {
   var newArray=[]; //empty array
+  /*This for loop iterates the size of array musician, using .push to add new
+  array elements to the empty array*/
   for(var i=0; i<musician.length; i++)
   {
     newArray.push(`${musician[i]} plays ${instrument[i]}`);
   }
   return newArray;
-
 }

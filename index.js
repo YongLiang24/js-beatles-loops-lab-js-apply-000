79221f9a5f0a06d5303bb79d800168d2bbe42f60
yong liang <yongliang24@gmail.com>
2018-11-03 07:55:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 // add solution here
+//case 1
 function theBeatlesPlay(musician, instrument)
 {
   var newArray=[]; //empty array
@@ -6,7 +7,19 @@ function theBeatlesPlay(musician, instrument)
   array elements to the empty array*/
   for(var i=0; i<musician.length; i++)
   {
+    //Use string interpolation
     newArray.push(`${musician[i]} plays ${instrument[i]}`);
   }
   return newArray;
+}
+//case 2
+function johnLennonFacts(facts){
+  var arr =[];
+  var c=0;
+  while(c <facts.length){
+    arr.push(facts[c] + '!!!');
+    c++;
+  }
+ return arr;
+  
 }

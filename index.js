99c23b5251b3aaecf8046c6877a1460889a834e1
yong liang <yongliang24@gmail.com>
2018-11-03 06:43:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ function theBeatlesPlay(arrayMusician, arrayInstrument)
   var empArray = [];
   for(i=0; i<arrayMusician.length; i++)
   {
-    empArray[i] = arrayMusician[i] + "plays" + arrayInstrument[i];
+    empArray[i] += arrayMusician[i] + "plays" + arrayInstrument[i];
   }
   return empArray;
 }

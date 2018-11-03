92021d38b92722e4726087eeaabc37110e2c3ab0
yong liang <yongliang24@gmail.com>
2018-11-03 07:31:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,16 +1,11 @@
 // add solution here
-function theBeatlesPlay(musicians, instruments)
+function theBeatlesPlay(musician, instrument)
 {
-  var string = [];
-  //create an array size of musician's length
-  for(i=0; i<musicians.length; i++)
+  var newArray=[]; //empty array
+  for(var i=0; i<musician.length; i++)
   {
-    string[i]= "";
+    newArray.push(`${musician[i]} plays ${instrument[i]}`);
   }
-  //
-  for(j=0; j<musicians.length; j++)
-  {
-    string[j]+="${musicians[j]} plays ${instruments[i]}";
-  }
-  return string;
+  return newArray;
+
 }
